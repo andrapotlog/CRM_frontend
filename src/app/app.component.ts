0596e37eb074ab4d791b409795baaea7a32e5211
APO19877 <APO19877@open-groupe.com>
2024-06-20 23:19:52 +0300

    load user in app component initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './service/auth/auth.service';
-import {Store} from "@ngrx/store";
-import {loadUser} from "./service/user-service/user.actions";
+import { Store } from '@ngrx/store';
+import { loadUser } from './service/user-service/user.actions';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +11,14 @@ import {loadUser} from "./service/user-service/user.actions";
 export class AppComponent implements OnInit {
   title = 'CRM';
 
-  constructor(private authService: AuthService, private store: Store) {}
+  constructor(
+    private authService: AuthService,
+    private store: Store,
+  ) {}
 
   ngOnInit(): void {
     this.authService.initAuthListener();
-    this.store.dispatch(loadUser());
+    if (localStorage.getItem('isAuth') === 'yes')
+      this.store.dispatch(loadUser());
   }
 }
